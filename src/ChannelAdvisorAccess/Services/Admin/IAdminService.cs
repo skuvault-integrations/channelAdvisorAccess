53cab773087a0264fdb5c6d8c945d3fd23432c5d
Dmitry <d1984v@gmail.com>
2012-07-27 22:41:46 +0300

    forgotten new files

---
+++ src/ChannelAdvisorAccess/Services/Admin/IAdminService.cs
@@ -0,0 +1,27 @@
+﻿using ChannelAdvisorAccess.AdminService;
+
+namespace ChannelAdvisorAccess.Services.Admin
+{
+	public interface IAdminService
+	{
+		/// <summary>
+		/// Gets all authorization records
+		/// </summary>
+		/// <returns>List of authorization records</returns>
+		AuthorizationResponse[] GetAuthorizationList();
+
+		/// <summary>
+		/// Gets authorization record which corresponds to the specified ChannelAdvisor Account
+		/// </summary>
+		/// <param name="localId">A number which uniquely identifies a ChannelAdvisor Account</param>
+		/// <returns>List with a single authorization record</returns>
+		AuthorizationResponse[] GetAuthorizationList( string localId );
+
+		/// <summary>
+		/// Allows to request access to a specific ChannelAdvisor Account
+		/// </summary>
+		/// <param name="localId">A number which uniquely identifies a ChannelAdvisor Account</param>
+		/// <returns>True if the request was created. False otherwise</returns>
+		bool RequestAccess( int localId );
+	}
+}
