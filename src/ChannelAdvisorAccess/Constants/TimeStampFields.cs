a389f2856ce9d587b19a727fc3cd81273e5a5c51
John Dixon <john.dixon@skuvault.com>
2015-01-29 11:14:46 -0500

    ca: added string constants for specifying time stamp field.

---
+++ src/ChannelAdvisorAccess/Constants/TimeStampFields.cs
@@ -0,0 +1,9 @@
+﻿namespace ChannelAdvisorAccess.Constants
+{
+	public static class TimeStampFields
+	{
+		public const string CreateDate = "CreateDate";
+		public const string LastUpdateDate = "LastUpdateDate";
+		public const string QtyLastModifiedDate = "QtyLastModifiedDate";
+	}
+}
