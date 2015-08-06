3de29983f518887794e767a743bf1c7bf8f41439
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2015-08-06 17:47:30 +0300

    Added orders test

---
+++ src/ChannelAdvisorAccessTests/Orders/OrdersServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using ChannelAdvisorAccess.Constants;
+using ChannelAdvisorAccess.OrderService;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ChannelAdvisorAccessTests.Inventory
+{
+	public class OrdersServiceTests: TestsBase
+	{
+		[ Test ]
+		public async Task GetOrdersAsync()
+		{
+			var criteria = new OrderCriteria
+			{
+				StatusUpdateFilterBeginTimeGMT = DateTime.Now.AddHours( -1 ),
+				StatusUpdateFilterEndTimeGMT = DateTime.Now,
+				DetailLevel = DetailLevelTypes.Complete
+			};
+			var result = await this.OrdersService.GetOrdersAsync< OrderResponseDetailComplete >( criteria );
+
+			result.Should().NotBeEmpty();
+		}
+	}
+}
