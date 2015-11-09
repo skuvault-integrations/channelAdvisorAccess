e4b7385eaab05a35ce1be53af5aeb6e30691f12b
MaxKits <maxim.kitsenko.r31@gmail.com>
2015-11-10 00:28:14 +0300

    AssignLabelListToItemList log added

--- src/ChannelAdvisorAccess/Services/Items/ItemsService.cs
+++ src/ChannelAdvisorAccess/Services/Items/ItemsService.cs
@@ -1657,11 +1657,29 @@ namespace ChannelAdvisorAccess.Services.Items
 		{
 			Condition.Requires( labels, "labels" ).IsShorterOrEqual( 3, "Only up to 3 labels allowed." ).IsNotNull();
 
-			skus.DoWithPages( 500, s => AP.Submit.Do( () =>
+			if( mark.IsBlank() )
+				mark = Mark.CreateNew();
+
+			var parameters = new { labels, createLabelIfNotExist, skus, reason };
+			try
 			{
-				var resultOfBoolean = this._client.AssignLabelListToInventoryItemList( this._credentials, this.AccountId, labels, createLabelIfNotExist, s.ToArray(), reason );
-				CheckCaSuccess( resultOfBoolean );
-			} ) );
+				ChannelAdvisorLogger.LogTraceStarted( this.CreateMethodCallInfo( mark : mark, additionalInfo : this.AdditionalLogInfoString, methodParameters : parameters.ToJson() ) );
+
+				skus.DoWithPages( 500, s => AP.Submit.Do( () =>
+				{
+					ChannelAdvisorLogger.LogTraceRetryStarted( this.CreateMethodCallInfo( mark : mark, additionalInfo : this.AdditionalLogInfoString, methodParameters : s.ToJson() ) );
+					var resultOfBoolean = this._client.AssignLabelListToInventoryItemList( this._credentials, this.AccountId, labels, createLabelIfNotExist, s.ToArray(), reason );
+					CheckCaSuccess( resultOfBoolean );
+					ChannelAdvisorLogger.LogTraceRetryEnd( this.CreateMethodCallInfo( mark : mark, methodResult : resultOfBoolean.ToJson(), additionalInfo : this.AdditionalLogInfoString, methodParameters : s.ToJson() ) );
+				} ) );
+				ChannelAdvisorLogger.LogTraceEnd( this.CreateMethodCallInfo( mark : mark, additionalInfo : this.AdditionalLogInfoString, methodParameters : parameters.ToJson() ) );
+			}
+			catch( Exception exception )
+			{
+				var channelAdvisorException = new ChannelAdvisorException( this.CreateMethodCallInfo( mark : mark, additionalInfo : this.AdditionalLogInfoString ), exception );
+				ChannelAdvisorLogger.LogTraceException( channelAdvisorException );
+				throw channelAdvisorException;
+			}
 		}
 
 		public async Task AssignLabelListToItemListAsync( string[] labels, bool createLabelIfNotExist, IEnumerable< string > skus, string reason, Mark mark = null )
