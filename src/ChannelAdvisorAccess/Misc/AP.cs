b495326c0fab88f53de7a96b8d1088b9c5ce57d0
Slav Ivanyuk <slav.isv@gmail.com>
2015-12-02 22:32:04 -0500

    retry: added pause if get throttled to wait until the end of the hour (which is when request count gets reset)

--- src/ChannelAdvisorAccess/Misc/AP.cs
+++ src/ChannelAdvisorAccess/Misc/AP.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Net;
+using System.ServiceModel;
 using System.Threading.Tasks;
 using Netco.ActionPolicyServices;
 using Netco.Logging;
@@ -14,10 +16,10 @@ namespace ChannelAdvisorAccess.Misc
 		}
 
 		private static readonly ActionPolicy _query = ActionPolicy.Handle< Exception >().Retry( 10, ( ex, i ) =>
-			{
-				typeof( AP ).Log().Trace( ex, "Retrying CA API get call for the {0} time", i );
-				SystemUtil.Sleep( TimeSpan.FromSeconds( 0.5 + i ) );
-			} );
+		{
+			typeof( AP ).Log().Trace( ex, "Retrying CA API get call for the {0} time", i );
+			SystemUtil.Sleep( GetDelay( ex, i ) );
+		} );
 
 		public static ActionPolicyAsync QueryAsync
 		{
@@ -25,10 +27,10 @@ namespace ChannelAdvisorAccess.Misc
 		}
 
 		private static readonly ActionPolicyAsync _queryAsync = ActionPolicyAsync.Handle< Exception >().RetryAsync( 10, async ( ex, i ) =>
-			{
-				typeof( AP ).Log().Trace( ex, "Retrying CA API get call for the {0} time", i );
-				await Task.Delay( TimeSpan.FromSeconds( 0.5 + i ) );
-			} );
+		{
+			typeof( AP ).Log().Trace( ex, "Retrying CA API get call for the {0} time", i );
+			await Task.Delay( GetDelay( ex, i ) );
+		} );
 
 		public static ActionPolicy Submit
 		{
@@ -36,10 +38,10 @@ namespace ChannelAdvisorAccess.Misc
 		}
 
 		private static readonly ActionPolicy _sumbit = ActionPolicy.Handle< Exception >().Retry( 10, ( ex, i ) =>
-			{
-				typeof( AP ).Log().Trace( ex, "Retrying CA API submit call for the {0} time", i );
-				SystemUtil.Sleep( TimeSpan.FromSeconds( 0.5 + i ) );
-			} );
+		{
+			typeof( AP ).Log().Trace( ex, "Retrying CA API submit call for the {0} time", i );
+			SystemUtil.Sleep( GetDelay( ex, i ) );
+		} );
 
 		public static ActionPolicyAsync SubmitAsync
 		{
@@ -47,9 +49,45 @@ namespace ChannelAdvisorAccess.Misc
 		}
 
 		private static readonly ActionPolicyAsync _sumbitAsync = ActionPolicyAsync.Handle< Exception >().RetryAsync( 10, async ( ex, i ) =>
+		{
+			typeof( AP ).Log().Trace( ex, "Retrying CA API submit call for the {0} time", i );
+			await Task.Delay( GetDelay( ex, i ) );
+		} );
+
+		private static TimeSpan GetDelay( Exception ex, int retryNumber )
+		{
+			var webException = ex as WebException;
+			var delay = GetDelayForWebException( webException );
+			if( delay != null )
+				return delay.Value;
+
+			var protoclException = ex as ProtocolException;
+			if( protoclException != null )
 			{
-				typeof( AP ).Log().Trace( ex, "Retrying CA API submit call for the {0} time", i );
-				await Task.Delay( TimeSpan.FromSeconds( 0.5 + i ) );
-			} );
+				var pwe = protoclException.InnerException as WebException;
+				var delayPwe = GetDelayForWebException( pwe );
+				if( delayPwe != null )
+					return delayPwe.Value;
+			}
+
+			return TimeSpan.FromSeconds( 0.5 + retryNumber );
+		}
+
+		private static TimeSpan? GetDelayForWebException( WebException webException )
+		{
+			if( webException != null )
+			{
+				var response = webException.Response as HttpWebResponse;
+				if( response != null )
+				{
+					if( ( int )response.StatusCode == 429 )
+					{
+						var minutesLeftInTheHour = 62 - DateTime.UtcNow.Minute; // wait until current our ends + 2 extra minutes for buffer
+						return TimeSpan.FromMinutes( minutesLeftInTheHour );
+					}
+				}
+			}
+			return null;
+		}
 	}
 }
