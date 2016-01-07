9dfa6edaa7ab17ea52d03ef9279c156c5300aeb3
MaxKits <maxim.kitsenko.r31@gmail.com>
2016-01-07 17:37:41 +0300

    CreateMethodCallInfo moved to base class

--- src/ChannelAdvisorAccess/Services/Items/ItemsService.cs
+++ src/ChannelAdvisorAccess/Services/Items/ItemsService.cs
@@ -208,30 +208,5 @@ namespace ChannelAdvisorAccess.Services.Items
 				throw new ChannelAdvisorException( result.MessageCode, result.Message );
 		}
 		#endregion
-
-		private string CreateMethodCallInfo( string methodParameters = "", Mark mark = null, string errors = "", string methodResult = "", string additionalInfo = "", string notes = "", [ CallerMemberName ] string memberName = "" )
-		{
-			try
-			{
-				mark = mark ?? Mark.Blank();
-				var connectionInfo = this.ToJson();
-				var str = string.Format(
-					"{{Mark:\"{3}\", MethodName:{0}, ConnectionInfo:{1}, MethodParameters:{2} {4}{5}{6}{7}}}",
-					memberName,
-					connectionInfo,
-					string.IsNullOrWhiteSpace( methodParameters ) ? PredefinedValues.EmptyJsonObject : methodParameters,
-					mark,
-					string.IsNullOrWhiteSpace( errors ) ? string.Empty : ", Errors:" + errors,
-					string.IsNullOrWhiteSpace( methodResult ) ? string.Empty : ", Result:" + methodResult,
-					string.IsNullOrWhiteSpace( notes ) ? string.Empty : ",Notes: " + notes,
-					string.IsNullOrWhiteSpace( additionalInfo ) ? string.Empty : ", " + additionalInfo
-					);
-				return str;
-			}
-			catch( Exception exception )
-			{
-				return PredefinedValues.EmptyJsonObject;
-			}
-		}
 	}
 }
