afea61fba0a293f87893864dd053932519787efd
maxims-skuvault <maxim.strelsov@skuvault.com>
2020-08-26 11:08:23 -0400

    GUARD-742 Had to remove "?." operator since the build script can't handle it [v.8.7.3]

--- src/ChannelAdvisorAccess/REST/Shared/MultiPartResponseParser.cs
+++ src/ChannelAdvisorAccess/REST/Shared/MultiPartResponseParser.cs
@@ -10,9 +10,12 @@ namespace ChannelAdvisorAccess.REST.Shared
 	{
 		public static IEnumerable< T > Parse< T >( string response, out int httpBatchStatusCode )
 		{
-			var batchResponses = JsonConvert.DeserializeObject< BatchResponses< T > >( response )?.Responses;
-			httpBatchStatusCode = batchResponses?.ToList().Last().Status ?? ( int ) HttpStatusCode.OK;
-			return batchResponses?.ToList().Select( x => x.Body ) ?? new List<T>();
+			var batchResponses = JsonConvert.DeserializeObject< BatchResponses< T > >( response );
+			var batchResponsesList = ( batchResponses != null && batchResponses.Responses != null ) ? 
+				batchResponses.Responses.ToList() : new List< BatchResponse< T > >();
+			var lastResponseItem = batchResponsesList.LastOrDefault();
+			httpBatchStatusCode = ( lastResponseItem != null ) ? lastResponseItem.Status : ( int ) HttpStatusCode.OK;
+			return batchResponsesList.Select( x => x.Body ) ?? new List< T >();
 		}
 	}
 }
