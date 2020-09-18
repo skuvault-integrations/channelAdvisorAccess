35b3fd42ae834b8b6de29485d75f2ae46e993e53
maxims-skuvault <maxim.strelsov@skuvault.com>
2020-09-18 16:36:57 -0400

    GUARD-753 Updated version to 8.8.1

--- src/Global/GlobalAssemblyInfo.cs
+++ src/Global/GlobalAssemblyInfo.cs
@@ -23,4 +23,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 // Keep in track with CA API version
-[ assembly : AssemblyVersion( "8.8.0.0" ) ]
+[ assembly : AssemblyVersion( "8.8.1.0" ) ]
