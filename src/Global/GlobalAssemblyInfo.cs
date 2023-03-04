ede1bb981bebe6346f1244253b814b5fa3cd4250
maxim-skuvault <maxim.strelsov@skuvault.com>
2023-03-03 22:55:15 -0500

    GUARD-2774 v.8.17.1.4

--- src/Global/GlobalAssemblyInfo.cs
+++ src/Global/GlobalAssemblyInfo.cs
@@ -23,5 +23,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 // Keep in track with CA API version
-[ assembly : AssemblyVersion( "8.17.1.3" ) ]
+[ assembly : AssemblyVersion( "8.17.1.4" ) ]
 [ assembly : AssemblyFileVersion( "8.17.1" ) ]
